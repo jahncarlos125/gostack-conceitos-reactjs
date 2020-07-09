cef03aca45120c114d41e5aa01b41d0cbc17da8f
Jahn Carlos <jahn.santana@gmail.com>
2020-07-09 00:49:11 -0300

    conclui desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from "react";
+/* eslint-disable no-unused-expressions */
 
+import React, {useState, useEffect} from "react";
+
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const { data } = await api.post('repositories', {
+        title: `React-Native - ${Date.now()}`, 
+        url: 'https://github.com/facebook/react-native', 
+        techs: [
+          'JavaScript', 
+          'Java'
+        ]
+      });
+      setRepositories([...repositories, data]);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`)
+      const newRepositories = repositories.filter(repository => repository.id !== id);
+      setRepositories(newRepositories);
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  async function getRepositories(){
+    try {
+      const { data } = await api.get('repositories');
+      setRepositories(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getRepositories();
+  }, [])
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => 
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
